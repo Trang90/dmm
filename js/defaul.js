1f48ebb3a88da435b306e0156cf356e8f0373a79
Trang90 <65718423+Trang90@users.noreply.github.com>
2020-05-21 16:23:38 +0200

    test

---
+++ js/defaul.js
@@ -0,0 +1,12 @@
+	
+function menumobile()
+	{
+		if($(".navi-middle").css('display')=='none')
+		{
+			$(".navi-middle").fadeIn("fast");
+		}
+		else
+		{
+			$(".navi-middle").fadeOut("fast");
+		}
+	}
